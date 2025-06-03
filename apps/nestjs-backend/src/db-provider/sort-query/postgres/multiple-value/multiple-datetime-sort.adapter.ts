f2b10d34cee92062de837aa23d46d558c803ba15
Aries X <caoxing9@gmail.com>
2025-06-03 17:47:48 +0800

    fix: sort relative (#1569)
    * fix: date time sort precise should be microsecond
    
    * perf: increase sort transaction time when manual sort
    
    * perf: multiple datetime sort
    
    * fix: sqlite sort query
    
    * chore: delete extra code

--- apps/nestjs-backend/src/db-provider/sort-query/postgres/multiple-value/multiple-datetime-sort.adapter.ts
+++ apps/nestjs-backend/src/db-provider/sort-query/postgres/multiple-value/multiple-datetime-sort.adapter.ts
@@ -1,85 +1,67 @@
-import type { DateFormattingPreset, IDateFieldOptions } from '@teable/core';
 import type { Knex } from 'knex';
-import { getPostgresDateTimeFormatString } from '../../../group-query/format-string';
 import { SortFunctionPostgres } from '../sort-query.function';
 
 export class MultipleDateTimeSortAdapter extends SortFunctionPostgres {
   asc(builderClient: Knex.QueryBuilder): Knex.QueryBuilder {
-    const { options } = this.field;
-    const { date, time, timeZone } = (options as IDateFieldOptions).formatting;
-    const formatString = getPostgresDateTimeFormatString(date as DateFormattingPreset, time);
-
     const orderByColumn = this.knex.raw(
       `
-      (SELECT to_jsonb(array_agg(TO_CHAR(TIMEZONE(?, CAST(elem AS timestamp with time zone)), ?)))
+      (SELECT to_jsonb(array_agg(elem))
       FROM jsonb_array_elements_text(??::jsonb) as elem) ->> 0
       ASC NULLS FIRST,
-      (SELECT to_jsonb(array_agg(TO_CHAR(TIMEZONE(?, CAST(elem AS timestamp with time zone)), ?)))
+      (SELECT to_jsonb(array_agg(elem))
       FROM jsonb_array_elements_text(??::jsonb) as elem)
       ASC NULLS FIRST
       `,
-      [timeZone, formatString, this.columnName, timeZone, formatString, this.columnName]
+      [this.columnName, this.columnName]
     );
     builderClient.orderByRaw(orderByColumn);
     return builderClient;
   }
 
   desc(builderClient: Knex.QueryBuilder): Knex.QueryBuilder {
-    const { options } = this.field;
-    const { date, time, timeZone } = (options as IDateFieldOptions).formatting;
-    const formatString = getPostgresDateTimeFormatString(date as DateFormattingPreset, time);
-
     const orderByColumn = this.knex.raw(
       `
-      (SELECT to_jsonb(array_agg(TO_CHAR(TIMEZONE(?, CAST(elem AS timestamp with time zone)), ?)))
+      (SELECT to_jsonb(array_agg(elem))
       FROM jsonb_array_elements_text(??::jsonb) as elem) ->> 0
       DESC NULLS LAST,
-      (SELECT to_jsonb(array_agg(TO_CHAR(TIMEZONE(?, CAST(elem AS timestamp with time zone)), ?)))
+      (SELECT to_jsonb(array_agg(elem))
       FROM jsonb_array_elements_text(??::jsonb) as elem)
       DESC NULLS LAST
       `,
-      [timeZone, formatString, this.columnName, timeZone, formatString, this.columnName]
+      [this.columnName, this.columnName]
     );
     builderClient.orderByRaw(orderByColumn);
     return builderClient;
   }
 
   getAscSQL() {
-    const { options } = this.field;
-    const { date, time, timeZone } = (options as IDateFieldOptions).formatting;
-    const formatString = getPostgresDateTimeFormatString(date as DateFormattingPreset, time);
-
     return this.knex
       .raw(
         `
-      (SELECT to_jsonb(array_agg(TO_CHAR(TIMEZONE(?, CAST(elem AS timestamp with time zone)), ?)))
+      (SELECT to_jsonb(array_agg(elem))
       FROM jsonb_array_elements_text(??::jsonb) as elem) ->> 0
       ASC NULLS FIRST,
-      (SELECT to_jsonb(array_agg(TO_CHAR(TIMEZONE(?, CAST(elem AS timestamp with time zone)), ?)))
+      (SELECT to_jsonb(array_agg(elem))
       FROM jsonb_array_elements_text(??::jsonb) as elem)
       ASC NULLS FIRST
       `,
-        [timeZone, formatString, this.columnName, timeZone, formatString, this.columnName]
+        [this.columnName, this.columnName]
       )
       .toQuery();
   }
 
   getDescSQL() {
-    const { options } = this.field;
-    const { date, time, timeZone } = (options as IDateFieldOptions).formatting;
-    const formatString = getPostgresDateTimeFormatString(date as DateFormattingPreset, time);
-
     return this.knex
       .raw(
         `
-      (SELECT to_jsonb(array_agg(TO_CHAR(TIMEZONE(?, CAST(elem AS timestamp with time zone)), ?)))
+      (SELECT to_jsonb(array_agg(elem))
       FROM jsonb_array_elements_text(??::jsonb) as elem) ->> 0
       DESC NULLS LAST,
-      (SELECT to_jsonb(array_agg(TO_CHAR(TIMEZONE(?, CAST(elem AS timestamp with time zone)), ?)))
+      (SELECT to_jsonb(array_agg(elem))
       FROM jsonb_array_elements_text(??::jsonb) as elem)
       DESC NULLS LAST
       `,
-        [timeZone, formatString, this.columnName, timeZone, formatString, this.columnName]
+        [this.columnName, this.columnName]
       )
       .toQuery();
   }
