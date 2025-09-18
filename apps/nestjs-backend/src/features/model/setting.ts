16ac6253ffe81e8081d9cdbf51382a1fc91fe4fa
Uno <uno@teable.io>
2025-09-18 14:11:48 +0800

    pref: setting cache (#1887)
    * feat:  add settings cache
    
    * refactor: add clearCache helper
    
    * fix: await clearCache

--- apps/nestjs-backend/src/features/model/setting.ts
+++ apps/nestjs-backend/src/features/model/setting.ts
@@ -3,13 +3,12 @@ import { PrismaService } from '@teable/db-main-prisma';
 import { ClsService } from 'nestjs-cls';
 import type { IPerformanceCacheStore } from '../../performance-cache';
 import { PerformanceCache, PerformanceCacheService } from '../../performance-cache';
-import { generateUserCacheKey } from '../../performance-cache/generate-keys';
+import { generateSettingCacheKey } from '../../performance-cache/generate-keys';
 import type { IClsStore } from '../../types/cls';
-import { dateToIso } from '../../utils/date-to-iso';
 import { clearCache } from './helper';
 
 @Injectable()
-export class UserModel {
+export class SettingModel {
   constructor(
     private readonly prismaService: PrismaService,
     private readonly performanceCacheService: PerformanceCacheService,
@@ -18,30 +17,26 @@ export class UserModel {
     this.prismaService.$use(async (params, next) => {
       const clearCacheKeys: (keyof IPerformanceCacheStore)[] = [];
       if (
-        params.model === 'User' &&
-        (params.action.includes('update') || params.action.includes('delete'))
+        params.model === 'Setting' &&
+        (params.action.includes('update') ||
+          params.action.includes('delete') ||
+          params.action.includes('upsert') ||
+          params.action.includes('create'))
       ) {
-        const whereId = params.args?.where?.id;
-        whereId && clearCacheKeys.push(generateUserCacheKey(whereId));
+        clearCacheKeys.push(generateSettingCacheKey());
       }
+
       await clearCache(params, clearCacheKeys, this.performanceCacheService, this.cls);
       return next(params);
     });
   }
 
   @PerformanceCache({
-    ttl: 30,
-    keyGenerator: generateUserCacheKey,
-    preventConcurrent: false,
-    statsType: 'user',
+    ttl: 60 * 60 * 24, // 1 day
+    keyGenerator: generateSettingCacheKey,
+    statsType: 'instance:setting',
   })
-  async getUserRawById(id: string) {
-    const res = await this.prismaService.txClient().user.findUnique({
-      where: { id, deletedTime: null },
-    });
-    if (!res) {
-      return null;
-    }
-    return dateToIso(res);
+  async getSetting() {
+    return await this.prismaService.txClient().setting.findMany();
   }
 }
