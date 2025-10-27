8e34dc63c636f9888059e4d80a4dc085ad6c0f5f
Pascal <mohammedpascal@gmail.com>
2025-10-27 23:29:17 +0300

    fixes

--- apps/nextjs-app/next.config.js
+++ apps/nextjs-app/next.config.js
@@ -14,6 +14,19 @@ const nextConfig = {
     '@teable/sdk',
     '@teable/ui-lib',
   ],
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'http',
+        hostname: 'localhost',
+        port: '3000',
+      },
+      {
+        protocol: 'https',
+        hostname: '*.successta.com',
+      },
+    ],
+  },
   async headers() {
     return [
       {
@@ -26,6 +39,31 @@ const nextConfig = {
         ],
       },
     ];
+  },
+  webpack: (config) => {
+    // Grab the existing rule that handles SVG imports
+    const fileLoaderRule = config.module.rules.find((rule) => rule.test?.test?.('.svg'));
+
+    config.module.rules.push(
+      // Reapply the existing rule, but only for svg imports ending in ?url
+      {
+        ...fileLoaderRule,
+        test: /\.svg$/i,
+        resourceQuery: /url/, // *.svg?url
+      },
+      // Convert all other *.svg imports to React components
+      {
+        test: /\.svg$/i,
+        issuer: fileLoaderRule.issuer,
+        resourceQuery: { not: [...fileLoaderRule.resourceQuery.not, /url/] },
+        use: ['@svgr/webpack'],
+      }
+    );
+
+    // Modify the file loader rule to ignore *.svg, since we have it handled now.
+    fileLoaderRule.exclude = /\.svg$/i;
+
+    return config;
   },
 };
 
