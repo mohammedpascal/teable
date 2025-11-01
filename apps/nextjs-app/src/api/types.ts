74c3b12b8994fc7356bdbe7332ad0105d914fb11
Pascal <mohammedpascal@gmail.com>
2025-11-01 15:52:00 +0300

    fixes

--- apps/nextjs-app/src/api/types.ts
+++ apps/nextjs-app/src/api/types.ts
@@ -9387,8 +9387,6 @@ export interface paths {
                 maxSizeAttachments: number;
                 maxNumDatabaseConnections: number;
                 maxRevisionHistoryDays: number;
-                maxAutomationHistoryDays: number;
-                automationEnable: boolean;
                 auditLogEnable: boolean;
                 adminPanelEnable: boolean;
                 rowColoringEnable: boolean;
@@ -9420,8 +9418,6 @@ export interface paths {
                 maxSizeAttachments: number;
                 maxNumDatabaseConnections: number;
                 maxRevisionHistoryDays: number;
-                maxAutomationHistoryDays: number;
-                automationEnable: boolean;
                 auditLogEnable: boolean;
                 adminPanelEnable: boolean;
                 rowColoringEnable: boolean;
@@ -9565,7 +9561,7 @@ export interface paths {
             /** Format: uri */
             homepage: string;
             logo?: string;
-            scopes?: (("table|create") | ("table|delete") | ("table|export") | ("table|import") | ("table|read") | ("table|update") | ("view|create") | ("view|delete") | ("view|read") | ("view|update") | ("field|create") | ("field|delete") | ("field|read") | ("field|update") | ("record|comment") | ("record|create") | ("record|delete") | ("record|read") | ("record|update") | ("automation|create") | ("automation|delete") | ("automation|read") | ("automation|update") | ("user|email_read"))[];
+            scopes?: (("table|create") | ("table|delete") | ("table|export") | ("table|import") | ("table|read") | ("table|update") | ("view|create") | ("view|delete") | ("view|read") | ("view|update") | ("field|create") | ("field|delete") | ("field|read") | ("field|update") | ("record|comment") | ("record|create") | ("record|delete") | ("record|read") | ("record|update") | ("user|email_read"))[];
             redirectUris: string[];
           };
         };
--- packages/core/src/auth/oauth.ts
+++ packages/core/src/auth/oauth.ts
@@ -1,36 +1,25 @@
-import type {
-  FieldAction,
-  RecordAction,
-  TableAction,
-  UserAction,
-  ViewAction,
-} from './actions';
+import type { FieldAction, RecordAction, TableAction, UserAction, ViewAction } from './actions';
 
-export const OAUTH_ACTIONS: (
-  | TableAction
-  | ViewAction
-  | FieldAction
-  | RecordAction
-  | UserAction
-)[] = [
-  'table|create',
-  'table|delete',
-  'table|export',
-  'table|import',
-  'table|read',
-  'table|update',
-  'view|create',
-  'view|delete',
-  'view|read',
-  'view|update',
-  'field|create',
-  'field|delete',
-  'field|read',
-  'field|update',
-  'record|comment',
-  'record|create',
-  'record|delete',
-  'record|read',
-  'record|update',
-  'user|email_read',
-];
+export const OAUTH_ACTIONS: (TableAction | ViewAction | FieldAction | RecordAction | UserAction)[] =
+  [
+    'table|create',
+    'table|delete',
+    'table|export',
+    'table|import',
+    'table|read',
+    'table|update',
+    'view|create',
+    'view|delete',
+    'view|read',
+    'view|update',
+    'field|create',
+    'field|delete',
+    'field|read',
+    'field|update',
+    'record|comment',
+    'record|create',
+    'record|delete',
+    'record|read',
+    'record|update',
+    'user|email_read',
+  ];
