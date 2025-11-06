20541f85ec56b1d7471ef41ed32641e5d4299e0f
Pascal <mohammedpascal@gmail.com>
2025-11-06 08:13:36 +0300

    fixes

--- apps/nestjs-backend/src/features/base/base.controller.ts
+++ apps/nestjs-backend/src/features/base/base.controller.ts
@@ -1,14 +1,4 @@
-import { Controller, Get, Query } from '@nestjs/common';
-import { baseQuerySchemaRo, IBaseQuerySchemaRo } from '@teable/openapi';
-import { ZodValidationPipe } from '../../zod.validation.pipe';
-import { BaseQueryService } from './base-query/base-query.service';
+import { Controller } from '@nestjs/common';
 
 @Controller('api/base/')
-export class BaseController {
-  constructor(private readonly baseQueryService: BaseQueryService) {}
-
-  @Get('bse0/query')
-  async sqlQuery(@Query(new ZodValidationPipe(baseQuerySchemaRo)) query: IBaseQuerySchemaRo) {
-    return this.baseQueryService.baseQuery(query.query, query.cellFormat);
-  }
-}
+export class BaseController {}
--- apps/nestjs-backend/src/features/base/base.module.ts
+++ apps/nestjs-backend/src/features/base/base.module.ts
@@ -4,12 +4,11 @@ import { FieldModule } from '../field/field.module';
 import { RecordModule } from '../record/record.module';
 import { TableOpenApiModule } from '../table/open-api/table-open-api.module';
 import { TableModule } from '../table/table.module';
-import { BaseQueryService } from './base-query/base-query.service';
 import { BaseController } from './base.controller';
 
 @Module({
   controllers: [BaseController],
   imports: [FieldModule, TableModule, TableOpenApiModule, RecordModule],
-  providers: [DbProvider, BaseQueryService],
+  providers: [DbProvider],
 })
 export class BaseModule {}
