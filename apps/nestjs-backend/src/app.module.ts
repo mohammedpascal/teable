fdededb16d6ae12c82eb0fcbdd89b93dd84532e3
Pascal <mohammedpascal@gmail.com>
2025-11-06 08:26:50 +0300

    fixes

--- apps/nestjs-backend/src/app.module.ts
+++ apps/nestjs-backend/src/app.module.ts
@@ -4,17 +4,20 @@ import { AccessTokenModule } from './features/access-token/access-token.module';
 import { AggregationOpenApiModule } from './features/aggregation/open-api/aggregation-open-api.module';
 import { AttachmentsModule } from './features/attachments/attachments.module';
 import { AuthModule } from './features/auth/auth.module';
-import { BaseModule } from './features/base/base.module';
 import { DashboardModule } from './features/dashboard/dashboard.module';
 import { ExportOpenApiModule } from './features/export/open-api/export-open-api.module';
+import { FieldModule } from './features/field/field.module';
 import { FieldOpenApiModule } from './features/field/open-api/field-open-api.module';
 import { HealthModule } from './features/health/health.module';
 import { ImportOpenApiModule } from './features/import/open-api/import-open-api.module';
 import { IntegrityModule } from './features/integrity/integrity.module';
 import { NextModule } from './features/next/next.module';
 import { NotificationModule } from './features/notification/notification.module';
+import { RecordModule } from './features/record/record.module';
 import { SelectionModule } from './features/selection/selection.module';
 import { SettingModule } from './features/setting/setting.module';
+import { TableOpenApiModule } from './features/table/open-api/table-open-api.module';
+import { TableModule } from './features/table/table.module';
 import { UserModule } from './features/user/user.module';
 import { GlobalModule } from './global/global.module';
 import { InitBootstrapProvider } from './global/init-bootstrap.provider';
@@ -27,7 +30,10 @@ export const appModules = {
     HealthModule,
     NextModule,
     FieldOpenApiModule,
-    BaseModule,
+    FieldModule,
+    TableModule,
+    TableOpenApiModule,
+    RecordModule,
     IntegrityModule,
     AttachmentsModule,
     WsModule,
