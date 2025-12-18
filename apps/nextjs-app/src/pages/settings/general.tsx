8dcbca396e9523019bff4bef032741382e41d986
Pascal <mohammedpascal@gmail.com>
2025-12-18 15:30:23 +0300

    fixes

--- apps/nextjs-app/src/pages/settings/general.tsx
+++ apps/nextjs-app/src/pages/settings/general.tsx
@@ -1,41 +1,11 @@
-import type { GetServerSideProps } from 'next';
 import type { ReactElement } from 'react';
 import type { ISettingPageProps } from '@/features/app/blocks/admin';
 import { SettingPage } from '@/features/app/blocks/admin';
 import { SettingLayout } from '@/features/app/layouts/SettingLayout';
-import ensureLogin from '@/lib/ensureLogin';
-import { getTranslationsProps } from '@/lib/i18n';
 import type { NextPageWithLayout } from '@/lib/type';
-import withAuthSSR from '@/lib/withAuthSSR';
-import withEnv from '@/lib/withEnv';
 
 const GeneralSettingsPage: NextPageWithLayout<ISettingPageProps> = ({ settingServerData }) => (
   <SettingPage settingServerData={settingServerData} />
-);
-
-export const getServerSideProps: GetServerSideProps = withEnv(
-  ensureLogin(
-    withAuthSSR(async (context, ssrApi) => {
-      const userMe = await ssrApi.getUserMe();
-
-      if (!userMe?.isAdmin) {
-        return {
-          redirect: {
-            destination: '/403',
-            permanent: false,
-          },
-        };
-      }
-
-      const setting = await ssrApi.getSetting();
-      return {
-        props: {
-          settingServerData: setting,
-          ...(await getTranslationsProps(context, 'common')),
-        },
-      };
-    })
-  )
 );
 
 GeneralSettingsPage.getLayout = function getLayout(page: ReactElement, pageProps) {
@@ -43,4 +13,3 @@ GeneralSettingsPage.getLayout = function getLayout(page: ReactElement, pageProps
 };
 
 export default GeneralSettingsPage;
-
