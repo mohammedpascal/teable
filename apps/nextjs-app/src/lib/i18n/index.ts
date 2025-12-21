0512ed1af049b0e409fa05e9e6e76947faeaca7f
Pascal <mohammedpascal@gmail.com>
2025-12-21 21:27:12 +0100

    fixes

--- apps/nextjs-app/src/lib/i18n/index.ts
+++ apps/nextjs-app/src/lib/i18n/index.ts
@@ -1,4 +1,2 @@
 export type { I18nNamespaces } from './I18nNamespaces';
 export type { I18nNamespace, I18nActiveNamespaces } from './I18nNamespace.types';
-export { getTranslationsProps } from './getTranslationsProps';
-export { getServerSideTranslations } from './getServerSideTranslations';
